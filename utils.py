1a37e9c52a76bb6eb8b9d9a3b0fb0aa76249cb6d
Danil Ovchinnikov <danil42worker@gmail.com>
2026-03-21 12:16:20 +0300

    updating dependencies and minor changes

--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
 import os
 import sys
+from pathlib import Path
 from typing import Any, TextIO
 
 
 class HiddenPrints:
+    devnull = Path(os.devnull)
+
     def __enter__(self) -> None:
         self._original_stdout: TextIO = sys.stdout
-        sys.stdout = open(os.devnull, "w")
+        sys.stdout = self.devnull.open("w")
 
     def __exit__(self, *args: Any) -> None:
         sys.stdout.close()
